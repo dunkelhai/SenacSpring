2e64f36a942812320ed0faf6848151bbdbacd36a
Alysson Oliveira <3oalysson@gmail.com>
2022-10-24 15:39:26 -0300

    ajustes

--- src/main/java/com/senac/SenacSpring/SenacSpringApplication.java
+++ src/main/java/com/senac/SenacSpring/SenacSpringApplication.java
@@ -2,8 +2,9 @@ package com.senac.SenacSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SenacSpringApplication {
 
 	public static void main(String[] args) {
